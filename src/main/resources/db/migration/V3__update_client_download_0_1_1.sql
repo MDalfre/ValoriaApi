08c25504ba155609f1c4b976ddf90e6851d94638
Marcio Henrique Dalfre <marciodalfre@gmail.com>
2026-07-23 10:57:15 -0300

    feat: publish Valoria client 0.1.1

---
+++ src/main/resources/db/migration/V3__update_client_download_0_1_1.sql
@@ -0,0 +1,6 @@
+UPDATE website.client_download
+SET url = 'https://github.com/MDalfre/ValoriaDocker/releases/download/client-v0.1.1/ValoriaClient-0.1.1.zip',
+    version = '0.1.1',
+    file_size = '445 MB',
+    checksum_sha256 = '7bd0db1c65f917876d25c37a10c48cb0a0b2397120aa317494b0f54d6673b062'
+WHERE id = '30000000-0000-0000-0000-000000000001';
