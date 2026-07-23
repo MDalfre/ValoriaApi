3a45fce0340868bf48af6a53229dffaeb3e600d4
Márcio Henrique Dalfré <marciodalfre@gmail.com>
2026-07-23 18:13:46 -0300

    chore: publish Valoria client 0.1.2

---
+++ src/main/resources/db/migration/V4__update_client_download_0_1_2.sql
@@ -0,0 +1,6 @@
+UPDATE website.client_download
+SET url = 'https://github.com/MDalfre/ValoriaDocker/releases/download/client-v0.1.2/ValoriaClient-0.1.2.zip',
+    version = '0.1.2',
+    file_size = '445 MB',
+    checksum_sha256 = 'c572f7e43f0264b95dad2509af78d192de14b8ea924200f4c800b99d58b088dc'
+WHERE id = '30000000-0000-0000-0000-000000000001';
